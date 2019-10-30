1f8f92a1a2b302b0be3a5e3470e3bd618448453f
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-10-30 23:34:43 +0100

    m-30, Merge finalisé pour la page d'accueil Ajout d'un peu de css pour rendre la page à peu prêt présentable. Encore un peu de css à faire Encore l'animation de Hover à faire

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,19 +5,19 @@ import "./MovieList.css";
 function MovieList(items) {
   return (
     <div>
-      <h2>Films américains</h2>
+      <h2 className="titleList">Films américains</h2>
       <div className="americanList">
       {items.items.movies.filter(movie => 
         movie.country === 'United_States')
-        .slice(0, 7)
+        .slice(0, 8)
         .map(movie => 
         <MovieCard {...movie} />)} 
       </div>
-      <h2>Films d'autres horizons</h2>
+      <h2 className="titleList">Films d'autres horizons</h2>
       <div className="frenchList">
       {items.items.movies.filter(movie =>
         movie.country !== 'United_States')
-        .slice(0, 7)
+        .slice(0, 8)
         .map(movie =>
         <MovieCard {...movie} />)}
       </div>
