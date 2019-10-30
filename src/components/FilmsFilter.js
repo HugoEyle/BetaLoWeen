e50dee4d53b2336679a8861713c1ede89b061e65
Hugo V <eyleteam@gmail.com>
2019-10-31 00:56:09 +0100

    bouton filter année fonctionnel

--- src/components/FilmsFilter.js
+++ src/components/FilmsFilter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './FilmsFilter.css';
 import Card from './Card';
 
-function FilmsFilter ({items, americanFilter, americanonly, frenchFilter, frenchonly, spainFilter, spainonly}) {
+function FilmsFilter ({items, americanFilter, americanonly, frenchFilter, frenchonly, spainFilter, spainonly, year2019Filter, year2019, year2018Filter, year2018, year2010Filter, year2010}) {
 	return (
 		<div className='filmsfilter'>
 			<div className='filter_title'>
@@ -17,9 +17,9 @@ function FilmsFilter ({items, americanFilter, americanonly, frenchFilter, french
 			</div>
 			<div className='filter_year'>
 				<p>Years :</p>
-				<button className='button_year'>2019</button>
-				<button className='button_year'>2018</button>
-				<button className='button_year'>2017</button>
+				<button className='button_year' onClick={year2019Filter}>2019</button>
+				<button className='button_year' onClick={year2018Filter}>2018</button>
+				<button className='button_year' onClick={year2010Filter}>2010</button>
 			</div>
 			{/* { americanonly ? '' : <img src={items.items.movies[14].posterUrl} alt='test' />} */}
 			<div className='testcard'>
@@ -35,6 +35,18 @@ function FilmsFilter ({items, americanFilter, americanonly, frenchFilter, french
 					: spainonly
 					? items.movies
 						.filter(movie => movie.country === 'Spain')
+						.map((movie, index) => <Card {...movie} />)
+					: year2019
+					? items.movies
+						.filter(movie => movie.year === 2019)
+						.map((movie, index) => <Card {...movie} />)
+					: year2018
+					? items.movies
+						.filter(movie => movie.year === 2018)
+						.map((movie, index) => <Card {...movie} />)
+					: year2010
+					? items.movies
+						.filter(movie => movie.year === 2010)
 						.map((movie, index) => <Card {...movie} />)
 					: items.movies.map((movie, index) => (
 							<Card {...movie} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -11,10 +11,16 @@ class Home extends React.Component {
       americanonly: false,
       frenchonly: false,
       spainonly: false,
+      year2019: false,
+      year2018: false,
+      year2010: false,
     }
     this.americanFilter = this.americanFilter.bind(this)
     this.frenchFilter = this.frenchFilter.bind(this)
     this.spainFilter = this.spainFilter.bind(this)
+    this.year2019Filter = this.year2019Filter.bind(this)
+    this.year2018Filter = this.year2018Filter.bind(this)
+    this.year2010Filter = this.year2010Filter.bind(this)
   }
 
   americanFilter() {
@@ -29,6 +35,18 @@ class Home extends React.Component {
     this.setState({ spainonly : !this.state.spainonly })
   }
 
+  year2019Filter() {
+    this.setState({ year2019 : !this.state.year2019 })
+  }
+
+  year2018Filter() {
+    this.setState({ year2018 : !this.state.year2018 })
+  }
+
+  year2010Filter() {
+    this.setState({ year2010 : !this.state.year2010 })
+  }
+
   componentDidMount () {
     fetch('http://hackathon-wild-hackoween.herokuapp.com/movies')
       .then(res => res.json())
@@ -41,7 +59,7 @@ class Home extends React.Component {
   
   render() {
     
-    const { isLoaded, items, americanonly, frenchonly, spainonly} = this.state
+    const { isLoaded, items, americanonly, frenchonly, spainonly, year2019, year2018, year2010} = this.state
     console.log(items)
     if (!isLoaded) {
       return <h2>Loading...</h2>;
@@ -50,7 +68,7 @@ class Home extends React.Component {
       return (
           <div className='Home'>
             <p>{items.movies[0].title}</p>
-            <FilmsFilter items={items} americanFilter={this.americanFilter} americanonly={americanonly} frenchFilter={this.frenchFilter} frenchonly={frenchonly} spainFilter={this.spainFilter} spainonly={spainonly} />
+            <FilmsFilter items={items} americanFilter={this.americanFilter} americanonly={americanonly} frenchFilter={this.frenchFilter} frenchonly={frenchonly} spainFilter={this.spainFilter} spainonly={spainonly} year2019Filter={this.year2019Filter} year2019={year2019} year2018Filter={this.year2018Filter} year2018={year2018} year2010Filter={this.year2010Filter} year2010={year2010} />
           </div>
       )
     }
